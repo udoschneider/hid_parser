b6e06ac3b4d649b0250f3ebe9e443cdf2012b2f9
Udo Schneider <Udo.Schneider@homeaddress.de>
2021-05-15 12:14:00 +0200

    Initial commit

---
+++ lib/hid_parser.ex
@@ -0,0 +1,18 @@
+defmodule HidParser do
+  @moduledoc """
+  Documentation for `HidParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HidParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hid_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule HidParserTest do
+  use ExUnit.Case
+  doctest HidParser
+
+  test "greets the world" do
+    assert HidParser.hello() == :world
+  end
+end
